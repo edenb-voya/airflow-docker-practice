6f66deac307ecd72e777229af26d45f93cc372f3
edenb-voya <Eric.Denbin@voyatek.com>
2025-01-22 18:33:15 -0500

    Updates docker-compose and webserver_config.py to fix flask-limiter issue

--- webserver_config.py
+++ webserver_config.py
@@ -1,11 +1,11 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-....
 
-limiter = Limiter(
-  get_remote_address,
-  app=app,
-  storage_uri="redis://airflow-redis:6379/0",
-  storage_options={"socket_connect_timeout": 30},
-  strategy="fixed-window", # or "moving-window"
+def setup_limiter(app):
+    limiter = Limiter(
+        get_remote_address,
+        app=app,
+        storage_uri="redis://airflow-redis:6379/0",
+        storage_options={"socket_connect_timeout": 30},
+        strategy="fixed-window", # or "moving-window"
 )
