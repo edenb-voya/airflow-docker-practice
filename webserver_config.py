64f7d83b9d30b2d8483a496a8a9ea68f0a2f8e7e
edenb-voya <Eric.Denbin@voyatek.com>
2025-01-23 12:00:14 -0500

    Updates Redis service image to 7.2-bookworm

--- webserver_config.py
+++ webserver_config.py
@@ -10,9 +10,6 @@ logger = logging.getLogger(__name__)
 
 logger.debug("webserver_config.py is being loaded")
 
-# Create a Redis client
-redis_client = Redis(host='airflow-redis', port=6379, db=0)
-
 # Configure Flask-Limiter to use Redis as the storage backend
 def configure_limiter(app):
     logger.debug("Configuring Flask-Limiter with Redis")
@@ -28,9 +25,8 @@ def configure_limiter(app):
 # Apply the configuration to the Flask app
 def init_app(app):
     logger.debug("Initializing Flask app with custom configuration")
-    app = create_app()
     configure_limiter(app)
-    app.config['RATELIMIT_STORAGE_URL'] = "redis://airflow-redis:6379/0"
+    app.config['RATELIMIT_STORAGE_URI'] = "redis://airflow-redis:6379/0"
     return app
 
 # Initialize the Flask app
