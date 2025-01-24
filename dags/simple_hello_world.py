813883365f47707f2f83d1a6a567d630b6aae242
edenb-voya <Eric.Denbin@voyatek.com>
2025-01-24 17:32:45 -0500

    Adds execute_multiple_tasks.py to the dags folder and codes it

--- dags/simple_hello_world.py
+++ dags/simple_hello_world.py
@@ -9,19 +9,18 @@ default_args = {
     'owner' : 'airflow',
 }
 
-dag = DAG(
+with DAG(
     dag_id = 'hello_world',
     description = 'Our first "Hello World" DAG!',
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = '@daily',
     tags = ['beginner', 'bash', 'hello world']
-)
+) as dag:
 
-task = BashOperator(
-    task_id = 'hello_world_task',
-    bash_command = 'echo Hello world once again!',
-    dag = dag
-)
+    task = BashOperator(
+        task_id = 'hello_world_task',
+        bash_command = 'echo Hello world once again!',
+    )
 
 task 
