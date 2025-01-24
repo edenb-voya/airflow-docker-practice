6a276fb18c54eb66dfb7a4bd25fde3c40aaf69dd
edenb-voya <Eric.Denbin@voyatek.com>
2025-01-24 18:54:46 -0500

    Updates execute_multiple_tasks.py with tasks E, F, and G

--- dags/execute_multiple_tasks.py
+++ dags/execute_multiple_tasks.py
@@ -15,44 +15,43 @@ with DAG(
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = timedelta(days=1),
-    tags = ['upstream', 'downstream']
+    tags = ['upstream', 'downstream'],
+    template_searchpath = 'C:\Users\Eric.Denbin\Repos\airflow-docker-practice\dags\bash_scripts'
 ) as dag:
 
     taskA = BashOperator(
         task_id = 'taskA',
-        bash_command = '''
-            echo TASK A has started!
-        
-            for i in {1..10}
-            do
-                echo TASK A printing $i
-            done
-        
-            echo TASK A has ended!
-        '''
+        bash_command = 'taskA.sh'
     )
 
     taskB = BashOperator(
         task_id = 'taskB',
-        bash_command = '''
-            echo TASK B has started!
-            sleep 4
-            echo TASK B has ended!
-        '''
+        bash_command = 'taskB.sh'
     )
 
     taskC = BashOperator(
         task_id = 'taskC',
-        bash_command = '''
-            echo TASK C has started!
-            sleep 15
-            echo TASK C has ended!
-        '''
+        bash_command = 'taskC.sh'
     )
 
     taskD = BashOperator(
         task_id = 'taskD',
-        bash_command = 'echo TASK D completed!'
+        bash_command = 'taskD.sh'
+    )
+
+    taskE = BashOperator(
+        task_id = 'taskE',
+        bash_command = 'taskE.sh'
+    )
+
+    taskF = BashOperator(
+        task_id = 'taskF',
+        bash_command = 'taskF.sh'
+    )
+
+    taskG = BashOperator(
+        task_id = 'taskG',
+        bash_command = 'taskG.sh'
     )
 
 taskA >> [taskB, taskC]
