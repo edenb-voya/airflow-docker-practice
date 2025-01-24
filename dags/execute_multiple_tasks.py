f214100b337ce9a62d4b61fbfafea0c26e8c4864
edenb-voya <Eric.Denbin@voyatek.com>
2025-01-24 18:21:51 -0500

    Updates execute_multiple_tasks.py to use bitshit operators for task dependencies

--- dags/execute_multiple_tasks.py
+++ dags/execute_multiple_tasks.py
@@ -55,8 +55,8 @@ with DAG(
         bash_command = 'echo TASK D completed!'
     )
 
-taskA.set_downstream(taskB)
-taskA.set_downstream(taskC)
+taskA >> taskB
+taskA >> taskB
 
-taskD.set_upstream(taskB)
-taskD.set_upstream(taskC)
+taskD << taskB
+taskD << taskC
