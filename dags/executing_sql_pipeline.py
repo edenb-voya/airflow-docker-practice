51a130c751907a4d8789f8e95f34d59b024142c4
edenb-voya <Eric.Denbin@voyatek.com>
2025-01-28 17:23:33 -0500

    Creates and codes executing_sql_pipeline.py

---
+++ dags/executing_sql_pipeline.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+from datetime import date, datetime, timedelta
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner' : 'airflow'
+}
+
+with DAG(
+    dag_id = 'executing_sql_pipeline',
+    description = 'Pipeline using SQL operators',
+    default_args = default_args,
+    start_date = days_ago(1),
+    schedule_interval = '@once',
+    tags = ['pipeline', 'sql']
+) as dag:
+    create_table = PostgresOperator(
+        task_id = 'create_table'
+        sql = r"""
+            CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL,
+                    age INTEGER NOT NULL,
+                    is_active BOOLEAN DEFAULT true,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        """,
+        postgres_conn_id = 'postgres_conn',
+        dag = dag
+    )
+
+create_table
